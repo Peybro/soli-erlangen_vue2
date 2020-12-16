ccf65ec76b3436aacef00772107493745e9521d4
Thomas Kretschmann <t.kretschmann@t-online.de>
2020-12-16 15:37:03 +0100

    changes

---
+++ src/components/Corona.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-success py-1">
+    <div class="container">
+      <b-alert show variant="warning">
+        <h4>Update zu Öffnungszeiten</h4>
+        <p>
+          Liebe Freunde des Sports, leider treffen die Maßnahmen gegen die
+          Pandemie auch uns, sodass wir gezwungen sind den Sportbetrieb bis auf
+          weiteres einzustellen.
+        </p>
+        <hr />
+        <p>
+          Wir wünschen euch und euren Familien eine gute und vor allem gesunde
+          Zeit, und hoffen, dass wir uns schon bald wieder sehen!
+        </p>
+      </b-alert>
+    </div>
+  </div>
+</template>
