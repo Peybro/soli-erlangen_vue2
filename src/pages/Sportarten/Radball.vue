7044fa0411af7fe9b6e4ed5d6c6ce531a9c8adb2
Thomas Kretschmann <t.kretschmann@t-online.de>
2020-12-16 15:19:23 +0100

    removed u17

--- src/pages/Sportarten/Radball.vue
+++ src/pages/Sportarten/Radball.vue
@@ -13,12 +13,28 @@
             <router-link to="/sportarten">
               <i class="material-icons m-2 float-right">alarm</i>
             </router-link>
-            <p>Donnerstag: 18:30 - 19:30 Uhr (Jugend)</p>
             <p>Donnerstag: 19:30 - 22:00 Uhr</p>
-            <p>Freitag: 16:30 - 21:00 Uhr (Jugend)</p>
+            <p>Freitag: 16:30 - 20:00 Uhr (Jugend)</p>
           </b-alert>
           <b-card no-body>
-            <b-tabs card active-nav-item-class="font-weight-bold text-uppercase text-danger">
+            <p>Radball gehört zur Sparte des Hallenradsports. Zwei Teams versuchen, den Ball mit Hilfe eines Radballrandes möglichst oft im Tor des Gegeners unterzubringen.</p>
+            <p>Diese Sportart erfordert von den Spielern eine hervorragende Bewegungsmotorik und Geschicklichkeit im Umgang mit dem Radballrad, das mit einem herkömmlichen Fahrrad nur wenig Ähnlichkeit hat. Es hat eine starre Übersetzung und keine Bremse.</p>
+            <p>Beim 2er Radball besteht eine Mannschaft aus zwei Spielern. Torwart und Feldspieler, wobei auch der Torhüter im Feld spielen kann und umgekehrt. Die Spielzeit beträgt je nach Altersklasse 2 x 5,6 oder 7 Minuten.</p>
+            <p>Das Spielfeld ist 14 Meter lang und 11 Meter breit. Es ist umgeben von einer 30 cm hohen Schrägbande. Die Tore sind zwei Meter breit und ebenso hoch.</p>
+            <p>Der Ball hat Kugelform, 17 - 18 cm Durchmesser und hat ein Gewicht zwischen 500 und 600 g. Seine Geschwindigkeit erreicht bei Freischlägen bis zu 65 km/h.</p>
+            <p>Gespielt wird der Ball durch Schläge mit dem Rad ohne Benutzung von Händen und Füßen. Nur der verteidigende Torwart darf in seinem Strafraum auch die Hände zur Ballabwehr benutzen. Kopfbälle sind gestattet.</p>
+            <p>Im Strafraum, dem mit einer durchzogegen Linie von 2-m-Radius gekennzeichneten Halbkreis um das Tor, darf sich jeweils nur ein Spieler jeder Mannschaft befinden. Grobe Regelverstöße im eigenen Strafraum (z.B. Fussabwehr) werden mit einem Penalty (4 Meter ) geahndet. Andere Regelübertretungen außerhalb des Strafraumes (z.B. Angriff auf den Gegner oder dessen Fahrrad) werden mit Freischlägen bestraft. Beim Ausball wird das Spiel nach Unterbrechung einen Meter von der Bande entfernt mit einem Abschlag fortgesetzt.</p>
+            <p>Bei einer Ecke, wie Fussball durch die verteidigende Mannschaft verursacht, wird der Ball von einem fixierten Punkt aus, zwei Meter von der verlängerten Torauslinie, geschlagen.</p>
+            <p>Bei Bodenberührung während des Spiels mit Fuß oder Körper ist der betreffende Spieler erst wieder spielberechtigt, wenn er die eigene Torauslinie überquert hat.</p>
+            <p>Besuchen Sie Radball-Events und Sie werden begeistert sein !</p>
+            <br>
+            <p>
+              ©
+              <a
+                href="https://www.wm-dornbirn2017.at/de/radball/"
+              >https://www.wm-dornbirn2017.at/de/radball/</a>
+            </p>
+            <!-- <b-tabs card active-nav-item-class="font-weight-bold text-uppercase text-danger">
               <b-tab title="Was ist Radball?" active>
                 <p>Radball gehört zur Sparte des Hallenradsports. Zwei Teams versuchen, den Ball mit Hilfe eines Radballrandes möglichst oft im Tor des Gegeners unterzubringen.</p>
                 <p>Diese Sportart erfordert von den Spielern eine hervorragende Bewegungsmotorik und Geschicklichkeit im Umgang mit dem Radballrad, das mit einem herkömmlichen Fahrrad nur wenig Ähnlichkeit hat. Es hat eine starre Übersetzung und keine Bremse.</p>
@@ -46,7 +62,7 @@
               <b-tab title="Unsere U15">
                 <TobiMoritz></TobiMoritz>
               </b-tab>
-            </b-tabs>
+            </b-tabs> -->
           </b-card>
         </div>
       </div>
